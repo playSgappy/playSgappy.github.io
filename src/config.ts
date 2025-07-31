559b3bb57baa3ca61a1aae5b60947f383f4c7310
playSgappy <123871479+playSgappy@users.noreply.github.com>
2025-08-01 06:01:43 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/banner1.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar1.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "playSgappy",
 	bio: "Congratulations!",
 	links: [
