4dfd8396f5f2533495db26a4c3c913a5d91f80f2
playSgappy <123871479+playSgappy@users.noreply.github.com>
2025-08-02 01:04:23 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://github.com/playSgappy/playSgappy.github.io/",
+	site: "https://playsgappy.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
