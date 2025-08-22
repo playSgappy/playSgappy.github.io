c9be8163be8db2cb11e2a761c9aac164353b3419
playSgappy <123871479+playSgappy@users.noreply.github.com>
2025-08-22 12:07:26 +0800

    Update link-presets.ts

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -7,6 +7,10 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 		name: i18n(I18nKey.home),
 		url: "/",
 	},
+	[LinkPreset.Friends]: {
+		name: i18n(I18nKey.friends),
+		url: "/friends/",
+	},
 	[LinkPreset.About]: {
 		name: i18n(I18nKey.about),
 		url: "/about/",
