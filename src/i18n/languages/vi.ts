ceae1e86faf0c226609895e41382681603ce4a89
playSgappy <123871479+playSgappy@users.noreply.github.com>
2025-08-22 12:42:53 +0800

    Update vi.ts

--- src/i18n/languages/vi.ts
+++ src/i18n/languages/vi.ts
@@ -3,6 +3,7 @@ import type { Translation } from "../translation";
 
 export const vi: Translation = {
 	[Key.home]: "Trang chủ",
+	[Key.friends]: "Bạn bè",
 	[Key.about]: "Giới thiệu",
 	[Key.archive]: "Kho bài",
 	[Key.search]: "Tìm kiếm",
